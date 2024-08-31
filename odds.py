8a4208e860305b84b7d93b2fa9abff6c23f0ea6f
BryceTownsendOpenly <bryce.townsend@openly.com>
2024-08-31 15:49:24 -0400

    Odds work

--- odds.py
+++ odds.py
@@ -1,15 +1,27 @@
 import requests
-import password.json as pw
+import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
+api_key = os.environ["ODDS_API_KEY"]
+sport_key= "americanfootball_nfl"
+REGIONS = 'us'
+MARKETS = 'spreads'
+ODDS_FORMAT = 'decimal' # decimal | american
+DATE_FORMAT = 'unix'
 
 odds_response = requests.get(
-    f'https://api.the-odds-api.com/v4/sports/{SPORT}/odds',
+    f'https://api.the-odds-api.com/v4/sports/{sport_key}/odds',
     params={
-        'api_key': API_KEY,
+        'api_key': api_key,
         'regions': REGIONS,
         'markets': MARKETS,
         'oddsFormat': ODDS_FORMAT,
         'dateFormat': DATE_FORMAT,
     }
 )
+
+with open('odds_response.json', 'w', encoding='utf-8') as f:
+    json.dump(odds_response.json(), f, ensure_ascii=False, indent=4)
