348ede86a96e19b0343aef1a2a92ab59d7b80877
BryceTownsendOpenly <bryce.townsend@openly.com>
2024-08-31 14:44:19 -0400

    checking

---
+++ odds.py
@@ -0,0 +1,15 @@
+import requests
+import password.json as pw
+
+
+
+odds_response = requests.get(
+    f'https://api.the-odds-api.com/v4/sports/{SPORT}/odds',
+    params={
+        'api_key': API_KEY,
+        'regions': REGIONS,
+        'markets': MARKETS,
+        'oddsFormat': ODDS_FORMAT,
+        'dateFormat': DATE_FORMAT,
+    }
+)
